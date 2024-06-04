3ecd6a02172ac53c61cb59118ff5873ae0312b0d
mobileexpert1 <appsdev096@gmail.com>
2024-06-04 12:59:26 +0530

    update commit

--- Package.swift
+++ Package.swift
@@ -21,12 +21,12 @@ let package = Package(
         .package(url: "https://github.com/aws-amplify/amplify-ui-swift-liveness", from: "1.2.11")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on. "Starscream", "SwiftEventBus",
+    ]
+//    targets: [
+//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+//        // Targets can depend on other targets in this package, and on products in packages this package depends on. "Starscream", "SwiftEventBus",
 //        .target(
-//            name: "iPass2.0NativeiOS",
+//           name: "iPass2.0NativeiOS",
 //           dependencies: ["DocumentReader", "DocumentReaderCore", "RegulaCommon" ,
 //            .product(name: "FaceLiveness", package: "amplify-ui-swift-liveness")
 //        ],
@@ -61,20 +61,20 @@ let package = Package(
 //              
 //               //.copy("Resource/DocumentReader.xcframework")
 //            ]),
-        .binaryTarget(
-            name: "DocumentReader",
-            path: "DocumentReader.xcframework"),
-        .binaryTarget(
-            name: "DocumentReaderCore",
-            path: "DocumentReaderCore.xcframework"),
-        .binaryTarget(
-            name: "RegulaCommon",
-            path: "RegulaCommon.xcframework"),
-        .testTarget(
-                 name: "iPass2.0NativeiOSTests",
-                  dependencies: ["iPass2.0NativeiOS"]),
-       
-    ]
+//        .binaryTarget(
+//            name: "DocumentReader",
+//            path: "DocumentReader.xcframework"),
+//        .binaryTarget(
+//            name: "DocumentReaderCore",
+//            path: "DocumentReaderCore.xcframework"),
+//        .binaryTarget(
+//            name: "RegulaCommon",
+//            path: "RegulaCommon.xcframework"),
+//        .testTarget(
+//                 name: "iPass2.0NativeiOSTests",
+//                  dependencies: ["iPass2.0NativeiOS"]),
+//       
+//    ]
     
 )
    
